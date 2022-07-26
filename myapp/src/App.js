5db830826917ef34ee9fc33fb87aaa45dd5b7802
Vishnu <vishnusingh1057@gmail.com>
2022-07-26 23:17:20 +0530

    FrontEnd Completed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from "./resources/mainpage/mainpage";
+import {Routes,Route} from "react-router-dom"
+import Contactus from "./resources/contact/contactus";
+import LoginPage from "./resources/loginpage";
+import SignupPage from "./resources/signup";
 
 function App() {
   return (
-    <div>
-      Hello World1
-    </div>
+
+      <div style={{
+          backgroundColor: `gray`,
+          height: '110vh',
+          width: '100vw'
+      }}>
+              <Routes>
+                  <Route  path="/" element={<MainPage/>}/>
+                  <Route  path='about' element={<Contactus/>}/>
+                  <Route  path='loginpage' element={<LoginPage/>}/>
+                  <Route  path='signup' element={<SignupPage/>}/>
+              </Routes>
+
+      </div>
+
   );
 }
 
